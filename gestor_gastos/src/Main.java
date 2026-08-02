dad5fbcd1d81db0eee62029feeded20662c8784f
ATSUSHINICA <josealejandrodelgadodiaz@gmail.com>
2026-08-02 18:39:38 +0200

    Modify Main class and add documentation comments

---
+++ gestor_gastos/src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Entry point of the application. Responsable for initializing and starting the program execution flow
+ * @author José Alejandro Díaz Delgado
+ * @since 2026/07/23
+ * */
+import modelo.Usuario;
+
+public class Main{
+    public static void main(String[] args){
+
+    }
+}
